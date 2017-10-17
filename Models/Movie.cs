396d04f8fa189fa9494a817fc6b3c4e0c6075332
Christian262 <christian.lautenschleger@gmail.com>
2017-10-17 17:16:40 -0400

    first commit

---
+++ Models/Movie.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Vidly.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+
+        [Required]
+        public Genre Genre { get; set; }
+
+        [Required]
+        [Display(Name = "Genre")]
+        public byte GenreId { get; set; }
+
+        [Required]
+        [Display(Name = "Release Date")]
+        public DateTime ReleaseDate { get; set; }
+
+        [Required]
+        public DateTime DateAdded { get; set; }
+
+        [Display(Name = "Number in Stock")]
+        [Required]
+        [Range(1, 25)]
+        public int NumberInStock { get; set; }
+
+        public byte NumberAvailable { get; set; }
+    }
+}
